62e2e174084e67262e2c5cc41f297a3bb2f7dcf4
sekizlipenguen <muratdemir3475@gmail.com>
2025-07-31 12:37:50 +0300

    Enhance `restartApp` method on iOS to increase reliability using multiple restart mechanisms.

--- ios/ReactNativeAppExit.h
+++ ios/ReactNativeAppExit.h
@@ -1,4 +1,5 @@
 #import <React/RCTBridgeModule.h>
+#import <UIKit/UIKit.h>
 
 @interface ReactNativeAppExit : NSObject <RCTBridgeModule>
 @property (nonatomic, weak) RCTBridge *bridge;
--- ios/ReactNativeAppExit.mm
+++ ios/ReactNativeAppExit.mm
@@ -1,5 +1,6 @@
 #import "ReactNativeAppExit.h"
 #import <React/RCTBridge.h>
+#import <React/RCTBundleURLProvider.h>
 
 @implementation ReactNativeAppExit
 
@@ -15,9 +16,31 @@ exit(0);
 
 // Uygulama restart fonksiyonu
 RCT_EXPORT_METHOD(restartApp) {
-    // Bridge'i yeniden yükleyerek uygulamayı yeniden başlatıyoruz
     dispatch_async(dispatch_get_main_queue(), ^{
+        // Birden fazla yöntemi aynı anda kullanarak restart işleminin başarılı olma olasılığını artırıyoruz
+
+        // 1. Yöntem: RCTTriggerReloadCommandNotification bildirimi gönderme
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"RCTTriggerReloadCommandNotification" object:nil];
+
+        // 2. Yöntem: RCTReloadNotification bildirimi gönderme
         [[NSNotificationCenter defaultCenter] postNotificationName:@"RCTReloadNotification" object:nil];
+
+        // 3. Yöntem: Bridge yeniden yükleme (eğer bridge varsa)
+        if (self.bridge) {
+            [self.bridge reload];
+        }
+
+        // 4. Yöntem: Uygulama delegesinin root view controller'ını yeniden yükleme
+        UIApplication *application = [UIApplication sharedApplication];
+        UIWindow *window = application.keyWindow;
+        if (window) {
+            UIViewController *rootViewController = window.rootViewController;
+            if (rootViewController) {
+                [rootViewController.view removeFromSuperview];
+                window.rootViewController = nil;
+                window.rootViewController = rootViewController;
+            }
+        }
     });
 }
 
